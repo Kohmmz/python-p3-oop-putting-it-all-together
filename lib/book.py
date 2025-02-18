29b233574eb057d40841a791652f1e6c95db7a22
Kohmmz <jeremiahkoome1@gmail.com>
2025-02-18 16:29:58 +0300

    Implement Book and Shoe classes with initialization and property validation

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title,page_count):
+      self.title = title
+      self.page_count = page_count
+
+    @property
+    def page_count(self):# COPY the current value of properties
+         return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):#check if the value being pass in okay
+         if isinstance(page_count, int):# check
+           self._page_count = page_count# where the value being passed in
+         else:
+           print("page_count must be an integer")
+    def turn_page(self):
+       print("Flipping the page...wow, you read fast!")
+
+
+
+
+
+
+
+
